333fab2aa7e949ea0255a2b486402bf3162a1e17
Hienpham1 <130559661+Hienpham1@users.noreply.github.com>
2025-05-28 12:21:04 +0700

    Update node_serial.py

--- src/robot_base_controller/robot_base_controller/node_serial.py
+++ src/robot_base_controller/robot_base_controller/node_serial.py
@@ -13,21 +13,22 @@ class ConnectNode(Node):
         # Declare parameters
         self.declare_parameter('port', '/dev/ttyACM0')
         self.declare_parameter('topic_velocities', 'topic_velocities')
-        self.declare_parameter('topic_son', 'topic_son')
+        self.declare_parameter('topic_paint', 'topic_paint')
         
         # Get parameters
         self.port = self.get_parameter('port').get_parameter_value().string_value
         self.topic_velocities = self.get_parameter('topic_velocities').get_parameter_value().string_value
-        self.topic_son = self.get_parameter('topic_son').get_parameter_value().string_value
+        self.topic_paint = self.get_parameter('topic_paint').get_parameter_value().string_value
         
         # Initialize variable
         self.ser = None
-        self.son_state = 0
+        self.paint_state = 0
         # Publishers and Subscribers
         self.velo_sub = self.create_subscription(Int16MultiArray, self.topic_velocities, self.velocities_callback, 10)
+        self.paint_sub = self.create_subscription(Int16MultiArray, self.topic_paint, self.paint_callback, 10)
         # Connect to serial
         self.connect_serial()
-        # Initialize motors on son
+        # Initialize motors on paint
         for _ in range(2):
             self.write_serial(1, 0, 0, 0)
         for _ in range(5):
@@ -41,32 +42,32 @@ class ConnectNode(Node):
             self.get_logger().error(f"Unable to open serial port {self.port}: {e}")
             time.sleep(5)
 
-    def write_serial(self, byte_reset, v_banh_trai, v_banh_phai, son_state):
-        dir_trai = 1 if v_banh_trai < 0 else 0
-        dir_phai = 1 if v_banh_phai < 0 else 0
-        v_banh_trai = abs(v_banh_trai)
-        v_banh_phai = abs(v_banh_phai)
-        v_banh_trai = min(v_banh_trai, 255)
-        v_banh_phai = min(v_banh_phai, 255)
+    def write_serial(self, byte_reset, pwm_left, pwm_right, paint_state):
+        dir_left = 1 if pwm_left < 0 else 0
+        dir_right = 1 if pwm_right < 0 else 0
+        pwm_left = abs(pwm_left)
+        pwm_right = abs(pwm_right)
+        pwm_left = min(pwm_left, 255)
+        pwm_right = min(pwm_right, 255)
 
-        chieu_trai = (dir_trai << 7) | 0x01
-        chieu_phai = (dir_phai << 7) | 0x02
+        dir_left = (dir_left << 7) | 0x01
+        dir_right = (dir_right << 7) | 0x02
 
         data = bytes([
         	byte_reset,
-        	chieu_trai,
-        	v_banh_trai,
-        	chieu_phai,
-        	v_banh_phai,
-                son_state
+        	dir_left,
+        	pwm_left,
+        	dir_right,
+        	pwm_right,
+            paint_state
     	])
         self.ser.write(data)
 
-    def son_callback(self, msg):
-        self.son_state = 1 if msg.data else 0
-        self.get_logger().info(f"Updated son state: {self.son_state}")
+    def paint_callback(self, msg):
+        self.paint_state = 1 if msg.data else 0
+        self.get_logger().info(f"Updated paint state: {self.paint_state}")
     def velocities_callback(self, msg):
-        self.write_serial(0, msg.data[0], msg.data[1], self.son_state)
+        self.write_serial(0, msg.data[0], msg.data[1], self.paint_state)
     
 def main(args=None):
     rclpy.init(args=args)
